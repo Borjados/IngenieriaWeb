c0d5732faa6c64349d43adcd91d5a2b9e6e3e727
Borjados <borja.diazdeotazu@opendeusto.es>
2020-10-16 19:44:00 +0200

    Initial commit

---
+++ project/misitio/myapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from myapp.models import Departamento,Habilidad,Empleado
+# Register your models here.
+
+admin.site.register(Departamento)
+admin.site.register(Habilidad)
+admin.site.register(Empleado)
