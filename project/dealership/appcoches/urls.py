a4075f9d9b5f8fccd3d46563412fc519b9f853b7
Borjados <borja.diazdeotazu@opendeusto.es>
2020-11-28 12:32:16 +0100

    commit plis

--- project/dealership/appcoches/urls.py
+++ project/dealership/appcoches/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
- path('', views.index, name='index'),
- path('auto/<int:coches_id>/', views.autos, name='autos')
+    path('', views.index, name='index'),
+    path('auto/<int:coches_id>/', views.autos, name='autos'),
+    path('marca/', views.marca, name='marca'),
+    path('categoria/', views.categoria, name='categoria'),
+    path('vendedor/', views.vendedor, name='vendedor')
 ]
--- project/dealership/appcoches/views.py
+++ project/dealership/appcoches/views.py
@@ -13,5 +13,20 @@ def autos(request, coches_id):
     autos = get_object_or_404(Coches, pk=coches_id)
     context = { 'autos': autos }
     return render(request, 'coches.html', context)
+    
+def marca(request):
+    marca = get_list_or_404(Marca.objects.order_by('nombre'))
+    context = { 'lista_marcas': marca}
+    return render(request, 'marca.html', context)
+    
+def categoria(request):
+    categoria = get_list_or_404(Categoria.objects.order_by('nombre'))
+    context = { 'lista_categorias': categoria}
+    return render(request, 'categoria.html', context)
+    
+def vendedor(request):
+    vendedor = get_list_or_404(Vendedor.objects.order_by('nombre'))
+    context = { 'lista_vendedores': vendedor}
+    return render(request, 'vendedor.html', context)
 
 # Create your views here.
